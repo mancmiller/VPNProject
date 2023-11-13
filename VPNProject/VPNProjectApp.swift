50d23e3135b7b858ef46a70415d5eb8401f134fd
mancmiller <manc.miller@gmail.com>
2023-11-13 17:16:56 +0600

    Initial Commit

---
+++ VPNProject/VPNProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  VPNProjectApp.swift
+//  VPNProject
+//
+//  Created by Muslim on 13.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct VPNProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
