a41585a4609bb35b76bb9246ff6b920c6d2fffe5
mancmiller <manc.miller@gmail.com>
2023-11-13 21:44:13 +0600

    Added assets and font

--- VPNProject/MenuView.swift
+++ VPNProject/MenuView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  MenuView.swift
 //  VPNProject
 //
 //  Created by Muslim on 13.11.2023.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct MenuView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    MenuView()
 }
